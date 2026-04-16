7400091c51df5d16b2eced35f22507d63efcc09d
Julian <julian.ocampo1219@gmail.com>
2026-04-16 20:19:48 -0300

    Class Imagen

---
+++ TP-WinForm-Programacion-3/TP-WinForm-Programacion3/Imagen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_WinForm_Programacion3
+{
+    internal class Imagen
+    {
+        private int Id;
+        private int IdArticulo;
+        private string ImgenUrl;
+
+    }
+}
