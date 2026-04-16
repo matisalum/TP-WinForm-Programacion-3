279b381472c1fa1e6d60a8727b3c00b4dcf66510
Matias Salum <matiussalum@gmail.com>
2026-04-16 19:30:47 -0300

    Clase Articulos

---
+++ TP-WinForm-Programacion-3/TP-WinForm-Programacion3/Articulos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_WinForm_Programacion3
+{
+    internal class Articulos
+    {
+        private int Id;
+        private string Codigo;
+        private string Nombre;
+        private string Descripcion;
+        private int IdMarca;
+        private int IdCategoria;
+        private decimal Precio;
+
+        public Articulos()
+        {
+        }
+    }
+}
